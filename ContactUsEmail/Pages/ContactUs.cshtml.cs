09814fa169e33ee72c5496d2743b0d7427591503
Eizenheim <mortein43@gmail.com>
2024-03-08 22:12:04 +0200

    Добавьте файлы проекта.

---
+++ ContactUsEmail/Pages/ContactUs.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactUs.Pages;
+
+public class ContactUsModel : PageModel
+{
+    private readonly IEmailService _emailService;
+
+    public ContactUsModel(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+
+    public async Task<IActionResult> OnGet(string subject, string body)
+    {
+        if (string.IsNullOrEmpty(subject) || string.IsNullOrEmpty(body))
+        {
+
+        }
+        else
+        {// Створіть об'єкт EmailDto з потрібними даними
+            EmailDto emailDto = new EmailDto
+            {
+                To = "cathrine.abernathy@ethereal.email",
+                Subject = subject,
+                Body = body
+            };
+
+            _emailService.SendEmail(emailDto);
+            TempData["Message"] = "Повідомлення надіслано";
+        }
+        // Поверніть результат
+        return Page();
+    }
+}
